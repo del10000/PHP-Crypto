bc5781f60f53fe12c9610c5bc99ccc8733bdf4ee
Delphin <batantoud@gmail.com>
2021-10-26 11:22:24 +0200

    first commit

---
+++ prog.php
@@ -0,0 +1,16 @@
+<?php
+
+function genererChaineAleatoire($longueur)
+{
+ $caracteres = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+ $longueurMax = strlen($caracteres);
+ $chaineAleatoire = '';
+ for ($i = 0; $i < $longueur; $i++)
+ {
+ $chaineAleatoire .= $caracteres[rand(0, $longueurMax - 1)];
+ }
+ return $chaineAleatoire.PHP_EOL;
+}
+
+// echo genererChaineAleatoire();
+echo genererChaineAleatoire(20);
