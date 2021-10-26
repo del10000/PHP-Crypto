b05ddb0695b184fd0da3c706a398f600ad4f9b7e
Delphin <batantoud@gmail.com>
2021-10-26 16:14:25 +0200

    add decrypt

--- prog.php
+++ prog.php
@@ -1,6 +1,7 @@
 <?php
 
-// function createWallet(){
+if ($argv[1] === "create" && $argv[2] === "wallet") {
+
     function genererChaineAleatoire($longueur){
         $caracteres = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
         $longueurMax = strlen($caracteres);
@@ -10,11 +11,30 @@
         }
         return $chaineAleatoire;
     }
-    
+
     $priv = genererChaineAleatoire(20);
     echo $priv.PHP_EOL;
     $public = md5($priv);
     echo $public.PHP_EOL;
+
+    $createFile = file_put_contents($argv[3] . "prog.json", json_encode([$priv, $public]));
+}
+
+if ($argv[1] === "decrypt"){
+    $block = file_get_contents("http://sophos.gg:7676/block");
+    $decode = json_decode($block);
+    $compare = $decode->{'block'};
     
-    $createFile = file_put_contents($argv[1] . "prog.json", json_encode([$priv, $public]));
-// }
+    for ($i = 0; $i < 100; $i++){
+    
+        $truc = strtoupper(md5($i));
+        $test = strtoupper(md5($truc));
+        if ($test === $compare) {
+    
+            echo($test).PHP_EOL;
+            echo("trouvé").PHP_EOL;
+            echo("le nombre était " . $i).PHP_EOL;
+            return;
+        }
+    }
+}
