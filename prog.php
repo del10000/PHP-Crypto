e911df7e431d4699d8d76b4b7c2c03463c95558a
Delphin <batantoud@gmail.com>
2021-10-26 14:39:01 +0200

    ...

--- prog.php
+++ prog.php
@@ -1,19 +1,20 @@
 <?php
 
-function genererChaineAleatoire($longueur){
-    $caracteres = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $longueurMax = strlen($caracteres);
-    $chaineAleatoire = '';
-    for ($i = 0; $i < $longueur; $i++){
-        $chaineAleatoire .= $caracteres[rand(0, $longueurMax - 1)];
+// function createWallet(){
+    function genererChaineAleatoire($longueur){
+        $caracteres = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $longueurMax = strlen($caracteres);
+        $chaineAleatoire = '';
+        for ($i = 0; $i < $longueur; $i++){
+            $chaineAleatoire .= $caracteres[rand(0, $longueurMax - 1)];
+        }
+        return $chaineAleatoire;
     }
-    return $chaineAleatoire;
-}
-
-$priv = genererChaineAleatoire(20);
-echo $priv.PHP_EOL;
-$public = md5($priv);
-echo $public.PHP_EOL;
-
-$json = json_encode([$priv, $public]);
-$create = file_put_contents("prog.json", $json);
+    
+    $priv = genererChaineAleatoire(20);
+    echo $priv.PHP_EOL;
+    $public = md5($priv);
+    echo $public.PHP_EOL;
+    
+    $createFile = file_put_contents($argv[1] . "prog.json", json_encode([$priv, $public]));
+// }
